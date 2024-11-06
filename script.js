96cb88dd7c0067cf91a533c58dbe313357caba11
hapssatou Sy <hapssatousy01@gmail.com>
2024-11-06 11:33:28 +0100

    modalJs

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 'use strict';
+//selection des éléments
+
+const btnsModal = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnClose = document.querySelector(".close-modal");
+
+function close() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+function open() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+for (let i = 0; i < btnsModal.length; i++) {
+  btnsModal[i].addEventListener("click", () => {
+    open();
+  });
+}
+
+overlay.addEventListener("click", () => {
+  close();
+});
+
+btnClose.addEventListener("click", (e) => {
+  close();
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    close();
+  }
+});
